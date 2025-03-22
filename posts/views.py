7fe04f5d3c644c02b8d5a8a78b3848cb12d287ab
princesshavy <havilah757@gmail.com>
2025-03-22 13:31:57 +0100

    i showed post to the user inside the app

--- posts/views.py
+++ posts/views.py
@@ -2,4 +2,12 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def helloWorld(request):
+    html = "" 
+    for post in posts :
+     html += f'''
+        <div>
+             <h1>{post.id} - {post.title}</h1>
+             <p>{post.content}</p>
+        </div>  
+    '''
     return HttpResponse("<h1>Hello World!</h1>")
