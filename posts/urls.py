b0804b4835fc72e7b0fe4c35731d9e7ce4bedc52
princesshavy <havilah757@gmail.com>
2025-03-29 13:58:02 +0100

    ran server

--- posts/urls.py
+++ posts/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('helloworld/',views.helloWorld),
     path("<int:id>/",views.post)
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,18 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+#import posts
 posts = [
     {
-        "id":0,
+        "id":1,
         "title":'Python is interpreted,high level,general purpose program',
+        "content":'Javascript is interpreted , high level,general purpose program'
     },
     {
-        "id":1,
+        "id":2,
         "title":'Let\'s explore Javascript',
         "content":'Javascript is interpreted , high level,general purpose program'
-    }
-    ]
-import posts
+    },
+]
 # Create your views here.
 def helloWorld(request):
     html = "" 
@@ -22,17 +22,17 @@ def helloWorld(request):
 
              <h1>{post['id']} - {post['title']}</h1>
              <p>{post['content']}</p>
-        </div>  
-    '''
+        </div>'''
     return HttpResponse(html)
 
 def post(request,id):
     for post in posts:
-        if post['id'] == id :
+        if post['id'] == id:
             post_dict = post 
             break
     html = f''' 
-    <h1>{post_dict['title']}</h1>
-    <p>{post_dict['content']}</p>'''
+           <h1>{post_dict['title']}</h1>
+           <p>{post_dict['content']}</p>
+        '''
     return HttpResponse(html)
 
